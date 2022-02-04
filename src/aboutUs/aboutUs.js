b8fc4cecd5f53815ff047ba1727da73378d45c79
Zachary Truong <zacharytruong@gmail.com>
2022-02-03 22:29:29 -0600

    working on aboutUs page

--- src/aboutUs/aboutUs.js
+++ src/aboutUs/aboutUs.js
@@ -1,6 +1,7 @@
 import {blocks} from './../blocks.js';
 import Logo from './resources/TEALAX-transparent.png';
 import backgroundImageSrc from './resources/home-bg-slideshow2.jpg';
+import firstColImgSrc from './resources/about-img.jpg';
 
 const aboutUs = ( function() {
   const _headerBlock = ( function() {
@@ -8,7 +9,7 @@ const aboutUs = ( function() {
     // Cache DOM
     const header = document.getElementById('header');
     header.style.backgroundImage = `linear-gradient(to left, rgba(194, 21, 0, .5) , rgba(255, 197, 0, .5)), url('${backgroundImageSrc}')`;
-
+    header.style.height = '600px';
     // Create Main Navigation
     const mainNAv = blocks.addElement('div', header, 'mainNav');
     const logoDiv = blocks.addElement('div', mainNAv, 'logoDiv');
@@ -47,8 +48,33 @@ const aboutUs = ( function() {
     // Cache DOM
     const main = document.getElementById('main');
     
-    
-   
+    // Main block primary header
+    const primaryHeader = blocks.addElement('h2', main, 'primaryHeader');
+    primaryHeader.textContent = 'Our Story';
+    primaryHeader.style.textTransform = 'uppercase';
+
+    // Main block sub header
+    const subHeader = blocks.addElement('h4', main, 'primaryHeader');
+    subHeader.textContent = 'Your delicious tea house since 2019'
+
+    // Main block content
+    const mainContent = blocks.addElement('div', main, 'mainContent')
+    mainContent.style.width = '70%';
+    mainContent.style.margin = '0 auto';
+
+    // Main block content first column
+    const firstCol = blocks.addElement('div', mainContent, 'firstCol');
+    firstCol.style.width = '30%';
+    const img = blocks.addElement('img', firstCol, 'firstColImg');
+    img.style.width = '100%';
+    img.src = firstColImgSrc;
+    const imgCap = blocks.addElement('p', firstCol, 'imgCap');
+    imgCap.textContent = 'We Make everthing with LOVE';
+
+    // Main block content second column
+    const secondCol = blocks.addElement('div', mainContent, 'secondCOl');
+    secondCol.style.width = '100%';
+
   });
   
   const _footerBlock = ( function() {
